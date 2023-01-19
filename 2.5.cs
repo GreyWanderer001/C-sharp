509f4f43d7d4cfa02046af505b9f19a3b8fc4b74
GreyWanderer001 <91175301+GreyWanderer001@users.noreply.github.com>
2023-01-19 11:47:33 +0200

    Create 2.5.cs

---
+++ 2.5.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        
+        float userAge = 0.0f;
+        
+        Console.Write("Ievadiet vecumu: ");
+        userAge = float.Parse(Console.ReadLine());
+        
+        float jupiterYears = 11.86f;
+        float jupiterAge = userAge * jupiterYears;
+        
+        float journeyToJupiter = 9.0f;
+        
+        float newEarthAge = userAge + journeyToJupiter;
+        float newJupiterAge = newEarthAge * jupiterYears;
+        
+        Console.WriteLine(jupiterAge);
+        Console.WriteLine(newEarthAge);
+        Console.WriteLine(newJupiterAge);
+    }
+}
