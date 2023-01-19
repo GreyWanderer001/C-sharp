38eb64219e888507f757b0d300960b2f644f299f
GreyWanderer001 <91175301+GreyWanderer001@users.noreply.github.com>
2023-01-19 12:29:33 +0200

    Update 2.8.cs

--- 2.8.cs
+++ 2.8.cs
@@ -7,15 +7,22 @@ public class HelloWorld
         double numberOne = 12932;
         double numberTwo = -2828472;
         
-        numberOne = Math.Sqrt(Math.Abs(numberOne));
-        numberTwo = Math.Sqrt(Math.Abs(numberTwo));
+        numberOne = Math.Sqrt(numberOne);
+        numberTwo = Math.Sqrt(numberTwo);
         
-        if (numberOne < numberTwo) {
+        if (Double.IsNaN(numberOne) && Double.IsNaN(numberTwo)) {
+            Console.WriteLine("Error");
+        } else if (Double.IsNaN(numberOne)) {
+            Console.WriteLine("numberTwo ir mazaks");
+        } else if (Double.IsNaN(numberTwo)) {
             Console.WriteLine("numberOne ir mazaks");
         } else {
-            Console.WriteLine("numberTwo ir mazaks");
-        }
-        
+            if (numberOne < numberTwo) {
+                Console.WriteLine("numberOne ir mazaks");
+            } else if (numberOne > numberTwo) {
+                Console.WriteLine("numberTwo ir mazaks");
+            }
+        }        
         Console.Write(numberOne + " " + numberTwo);
     }
 }
