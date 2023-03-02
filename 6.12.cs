087ba244dc88d6d44a93c13a3224868e20eb5791
GreyWanderer001 <91175301+GreyWanderer001@users.noreply.github.com>
2023-03-02 12:55:31 +0200

    Create 6.12.cs

---
+++ 6.12.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string[] spaceRocks = {"meteroid", "meteor", "meteorite"};
+
+    bool makesContact = Array.Exists(spaceRocks, (string s) => s == "meteorite");
+
+    if (makesContact) {
+      Console.WriteLine("At least one space rock has reached the Earth's surface!");
+    }
+  }
+  
+}
